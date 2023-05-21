63ded087f6fe3351c704085ef414d48b3abde74c
lexicon <56066597+lexicon25@users.noreply.github.com>
2023-05-21 00:16:06 -0700

    Update pack-list.sql

--- pack-list.sql
+++ pack-list.sql
@@ -1,5 +1,8 @@
 -- Letters Pack
-SELECT * FROM the_list WHERE CHAR_LENGTH() = 1;   -- gets single numbers as well if that ever exists (unlikely)
+SELECT * FROM the_list WHERE name LIKE '_' ORDER BY name;
 
 -- Creator Pack
 SELECT * FROM the_list WHERE name IN (SELECT creator FROM Demon);
+
+-- Season Pack
+SELECT * FROM the_list WHERE name LIKE '%season%' OR name LIKE '%winter%' OR name LIKE '%spring%' OR name LIKE '%autumn%' OR name LIKE '%summer%';
