d8ce14db689ae1e82d08eba1214fe933f7c8ae78
lexicon <56066597+lexicon25@users.noreply.github.com>
2023-09-16 15:46:44 -0700

    idk

--- pack-list.sql
+++
@@ -1,8 +0,0 @@
--- Letters Pack
-SELECT * FROM the_list WHERE name LIKE '_' ORDER BY name;
-
--- Creator Pack
-SELECT * FROM the_list WHERE name IN (SELECT creator FROM Demon);
-
--- Season Pack
-SELECT * FROM the_list WHERE name LIKE '%season%' OR name LIKE '%winter%' OR name LIKE '%spring%' OR name LIKE '%autumn%' OR name LIKE '%summer%';
